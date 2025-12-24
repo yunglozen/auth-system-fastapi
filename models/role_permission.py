12d0292430638f9e15bf8aa47186518810057b76
Vitaliy <vitases20034@mail.ru>
2025-12-24 23:33:53 +0300

    Updated role_permission.py

--- models/role_permission.py
+++ models/role_permission.py
@@ -5,5 +5,7 @@ from .base import BaseModel
 
 
 class RolePermission(BaseModel):
+    __tablename__ = "role_permissions"
+
     role_id: Mapped = mapped_column(ForeignKey("roles.id", ondelete="CASCADE"), primary_key=True)
     permission_id: Mapped = mapped_column(ForeignKey("permissions.id", ondelete="CASCADE"), primary_key=True)
