1c42ff49f58cdc77ba67945819c5250bb82e25e3
Vitaliy <vitases20034@mail.ru>
2025-12-24 23:06:59 +0300

    Created UserRole

---
+++ models/user_roles.py
@@ -0,0 +1,9 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import BaseModel
+
+
+class UserRole(BaseModel):
+    user_id: Mapped = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    role_id: Mapped = mapped_column(ForeignKey("roles.id", ondelete="CASCADE"), primary_key=True)
