2a4778521c498b163ac2c6a312efd562b74c0aa6
Vitaliy <vitases20034@mail.ru>
2025-12-24 23:33:17 +0300

    Updated user_roles.py

--- models/user_roles.py
+++ models/user_roles.py
@@ -5,5 +5,7 @@ from .base import BaseModel
 
 
 class UserRole(BaseModel):
+    __tablename__ = "user_roles"
+
     user_id: Mapped = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
     role_id: Mapped = mapped_column(ForeignKey("roles.id", ondelete="CASCADE"), primary_key=True)
