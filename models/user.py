8df76b374e721e4a85337c5a47108c58c27cf091
Vitaliy <vitases20034@mail.ru>
2025-12-24 22:55:45 +0300

    Updated user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
 from sqlalchemy import String, Boolean, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
 from .base import BaseModel
 
 
 class User(BaseModel):
-    __tablename__ = "users"
-
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String, nullable=False)
@@ -19,5 +17,7 @@ class User(BaseModel):
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
     created_at: Mapped = mapped_column(DateTime(timezone=True), server_default=func.now())
-
     updated_at: Mapped = mapped_column(DateTime(timezone=True), onupdate=func.now())
+
+    role = relationship("Role", secondary="user_role", back_populates="users", lazy="selectin")
+
