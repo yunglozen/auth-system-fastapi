aed836aae1146515f1aebf502bb4e4c71d23986d
Vitaliy <vitases20034@mail.ru>
2025-12-24 22:59:56 +0300

    Update resource.py

--- models/resource.py
+++ models/resource.py
@@ -1,12 +1,11 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import BaseModel
 
 
 class Resources(BaseModel):
-    __tablename__ = "resources"
-
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(15), unique=True, nullable=False)
 
+    permissions = relationship("Permission", back_populates="resource", cascade="all, delete-orphan")
