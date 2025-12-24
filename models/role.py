3aa8d18b55c3dedb7b9188115a3ca68f15827ee3
Vitaliy <vitases20034@mail.ru>
2025-12-24 22:57:52 +0300

    Updated role.py

--- models/role.py
+++ models/role.py
@@ -5,11 +5,16 @@ from .base import BaseModel
 
 
 class Role(BaseModel):
-    __tablename__ = "roles"
-
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     description: Mapped[str | None] = mapped_column(String(255))
 
-    users = relationship("User", secondary="user_roles", back_populates="rolse")
+    users = relationship("User", secondary="user_roles", back_populates="roles")
+    permissions = relationship(
+        "Permission",
+        secondary="role_permissions",
+        back_populates="roles",
+        lazy="selectin"
+    )
+
 
