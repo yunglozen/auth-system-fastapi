53b55e8deeaad486f4718dbb80034438cb3c3f30
Vitaliy <vitases20034@mail.ru>
2025-12-24 23:02:25 +0300

    Update permission.py

--- models/permission.py
+++ models/permission.py
@@ -1,13 +1,14 @@
 from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import BaseModel
 
 
 class Permissions(BaseModel):
-    __tablename__ = "permissions"
-
     id: Mapped[int] = mapped_column(primary_key=True)
     action: Mapped[str] = mapped_column(String(15), nullable=False)
 
     resource_id: Mapped[int] = mapped_column(ForeignKey("resource.id", ondelete="CASCADE"), nullable=False)
+
+    resource = relationship("Resource", back_populates="permissions")
+    roles = relationship("Role", secondary="role_permissions", back_populates="permissions")
