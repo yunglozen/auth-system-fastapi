2ebf3dacb50ff4ae5b9fdc0c047d7ad985fdd679
Vitaliy <vitases20034@mail.ru>
2025-12-25 15:54:26 +0300

    Created RevokedToken Model

---
+++ models/revoked_token.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+from datetime import datetime
+
+from .base import BaseModel
+
+
+class RevokedToken(BaseModel):
+    __tablename__ = "revoked_tokens"
+
+    id:  Mapped[int] = mapped_column(primary_key=True)
+    token: Mapped[str] = mapped_column(String, nullable=False)
+    revoked_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
