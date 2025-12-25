d7abdaa8bb7abd4a348c3cab075441bd02ae7479
Vitaliy <vitases20034@mail.ru>
2025-12-25 19:52:02 +0300

    Created Services Route

---
+++ routers/services.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends
+
+from .profile import require_permission, get_current_user
+from models.user import User
+
+router = APIRouter(prefix="/services", tags=["services"])
+
+
+@router.get("/manager_notes", dependencies=[Depends(require_permission("notes", "read"))])
+async def get_notes(
+     current_user: User = Depends(get_current_user)
+):
+    return {
+        "READ MANAGER NOTES": "YES"
+    }
+
+
+@router.put("/manager_notes", dependencies=[Depends(require_permission("notes", "write"))])
+async def update_notes(
+     current_user: User = Depends(get_current_user)
+):
+    return {
+        "WRITE MANAGER NOTES": "YES"
+    }
+
+
+@router.get("/admin_photos", dependencies=[Depends(require_permission("photos", "read"))])
+async def get_notes(
+     current_user: User = Depends(get_current_user)
+):
+    return {
+        "GET ADMIN PHOTOS": "YES"
+    }
