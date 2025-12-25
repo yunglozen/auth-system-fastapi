d19d7e111cdba458ed6c357fb892a9089037cd17
Vitaliy <vitases20034@mail.ru>
2025-12-25 11:51:11 +0300

    Created User schemas

---
+++ schemas/user.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    first_name: str | None = None
+    last_name: str | None = None
+    middle_name: str | None = None
+
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8)
+    password_repeat: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserUpdate(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    middle_name: str | None = None
+
+
+class UserRead(UserBase):
+    id: int
+    is_active: bool
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
